4b1dd5b79c62d493df71aad8bdd4e01adb0a1e56
cloud-dev-user <70812553+cloud-dev-user@users.noreply.github.com>
2022-08-12 17:21:06 +0530

    Create duckduckgo_first_Script.py

---
+++ duckduckgo_first_Script.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+def test_eight_components():
+    driver = webdriver.Chrome(service=ChromeService(executable_path=ChromeDriverManager().install()))
+
+    driver.get("https://duckduckgo.com/")
+
+    title = driver.title
+    assert "DuckDuckGo" in title
+
+    driver.implicitly_wait(10)
+
+    search_box = driver.find_element(by=By.NAME, value="q")
+    search_button = driver.find_element(by=By.ID, value="search_button_homepage")
+
+    search_box.send_keys("Selenium")
+    search_button.click()
+
+    search_box = driver.find_element(by=By.NAME, value="q")
+    value = search_box.get_attribute("value")
+    assert value == "Selenium"
+
+    driver.quit()
